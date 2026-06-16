459b110566d7ae32154b51caea6dc02b5add7b0a
sudopydev <sudopydev@gmail.com>
2026-06-16 14:48:59 +0300

    chore: bump application version to 1.0.1

--- app/src/main/java/com/adsblock/vpn/ui/screens/AboutScreen.kt
+++ app/src/main/java/com/adsblock/vpn/ui/screens/AboutScreen.kt
@@ -92,7 +92,7 @@ fun AboutScreen() {
         Spacer(modifier = Modifier.height(8.dp))
 
         Text(
-            text = stringResource(R.string.about_version, "1.0"),
+            text = stringResource(R.string.about_version, "1.0.1"),
             style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.SemiBold),
             color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.7f),
         )
